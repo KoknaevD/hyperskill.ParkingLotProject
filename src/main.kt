0e42e223558c954e0b38418e1902400ac93ef5e2
KoknaevD <89024464050@yandex.ru>
2020-08-21 23:03:18 +0500

    Yay! You've completed Stage 4/5!

--- src/main.kt
+++ src/main.kt
@@ -7,36 +7,49 @@ fun main() {
 }
 
 object Parking {
-    val spotCount = 20
-    val spots: MutableList<Spot> = mutableListOf()
-    var working = true
+    private val spots: MutableList<Spot> = mutableListOf()
+    private var working = true
     fun start(){
-        repeat(spotCount) {
-            spots.add(Spot(null))
-        }
-
         while (working) {
-            val action = scanner.next()!!
 
-            when (action) {
+            when (scanner.next()!!) {
                 "park" -> {
                     val regNumber = scanner.next()!!
                     val carColor = scanner.next()!!
-                    val freeSpot = findEmptySpot()
-                    if (freeSpot == -1) {
-                        println("Sorry, the parking lot is full.")
+                    if (spots.isEmpty())  {
+                        println("Sorry, a parking lot has not been created.")
                     } else {
-                        spots[freeSpot].car = Car(regNumber, carColor)
-                        println("$carColor car parked in spot ${freeSpot + 1}.")
+                        val freeSpot = findEmptySpot()
+                        if (freeSpot == -1) {
+                            println("Sorry, the parking lot is full.")
+                        } else {
+                            spots[freeSpot].car = Car(regNumber, carColor)
+                            println("$carColor car parked in spot ${freeSpot + 1}.")
+                        }
                     }
                 }
                 "leave" -> {
                     val spotNumber = scanner.nextInt()
-                    if (spots[spotNumber - 1].isEmpty) {
-                        println("There is no car in spot $spotNumber.")
+                    if (spots.isEmpty()) {
+                        println("Sorry, a parking lot has not been created.")
                     } else {
-                        println("Spot $spotNumber is free.")
-                        spots[spotNumber - 1].car = null
+                        if (spots[spotNumber - 1].isEmpty) {
+                            println("There is no car in spot $spotNumber.")
+                        } else {
+                            println("Spot $spotNumber is free.")
+                            spots[spotNumber - 1].car = null
+                        }
+                    }
+                }
+                "create" -> {
+                    val spotNumber = scanner.nextInt()
+                    createParking(spotNumber)
+                }
+                "status" -> {
+                    if (spots.isEmpty())  {
+                        println("Sorry, a parking lot has not been created.")
+                    } else {
+                        printStatus()
                     }
                 }
                 "exit" -> working = false
@@ -45,13 +58,30 @@ object Parking {
         }
     }
 
+    private fun printStatus(){
+        var empty = true
+        spots.forEachIndexed { index, spot ->
+            if (!spot.isEmpty) {
+                println("${index + 1} ${spot.car!!.regNumber} ${spot.car!!.carColor}")
+                empty = false
+            }
+        }
+        if (empty) println("Parking lot is empty.")
+    }
+
     private fun findEmptySpot(): Int {
         return spots.indexOfFirst { it.isEmpty } }
+
+    private fun createParking(count: Int) {
+        spots.clear()
+        repeat(count) {
+            spots.add(Spot(null))
+        }
+        println("Created a parking lot with $count spots.")
+    }
 }
 
-class Car(var regNumber: String, var carColor: String) {
-
-}
+data class Car(var regNumber: String, var carColor: String)
 
 class Spot(var car:Car?) {
     val isEmpty: Boolean
